15184eaa0dc9b912320f08d433432c0e210118cd
NatashaMurashev <nmurashev@gmail.com>
2012-10-17 19:45:34 -0700

    starting project!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eduscore::Application.config.secret_token = '59018be78dab5ff4aaf00cd0694135bae3ba596a2e874f1f83c4d6e45030ab5a0825edab912f6c98c2bef0b1ab475164caf848b75cd4811bfeffa5470eb1b604'
